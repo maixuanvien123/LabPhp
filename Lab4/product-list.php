6e4271ffd37986df8f9239d4283550a9ecffded0
maixuanvien123 <106679180+maixuanvien123@users.noreply.github.com>
2024-03-22 18:12:30 +0700

    bo sung truoc 6h30

---
+++ Lab4/product-list.php
@@ -0,0 +1,34 @@
+<?php
+require_once('config/product.class.php');
+include_once('header.php');
+$prods = Product::list_product();
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table border="1" class="table">
+                <tr>
+                    <td>Picture</td>
+                    <td>Product Name</td>
+                    <td>CateID</td>
+                    <td>Price</td>
+                    <td>Quantity</td>
+                    <td>Description</td>
+                </tr>
+                <?php foreach ($prods as $item): ?>
+                    <tr>
+                        <td><?php echo $item['Picture']; ?></td>
+                        <td><?php echo $item['ProductName']; ?></td>
+                        <td><?php echo $item['CateID']; ?></td>
+                        <td><?php echo $item['Price']; ?></td>
+                        <td><?php echo $item['Quantity']; ?></td>
+                        <td><?php echo $item['Description']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include_once('footer.php'); ?>
